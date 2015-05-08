86f0168722b6a93e6720478e9b429586415a5793
criemann <16criemann@iaa.edu.ec>
2015-05-08 08:24:47 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,19 @@
+name = "Carl Riemann"
+age = 33
+gpa = 4.0
+
+print name
+print age 
+print gpa
+
+print 'My name is', name, '. I am ' , age , 'years old.'
+
+print "In ten years, I will be" , age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7
+print 'My dog is' , ageInDogYears, 'years old in Dog Years.'
+print 'But my dog is' , ageInHumanYears, 'years old in Human Years.'
+MyNumber = 3 + 11*2
+print MyNumber
