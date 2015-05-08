a20420bce7232b6a17aa0ce70f7d3e21403c02ec
criemann <16criemann@iaa.edu.ec>
2015-05-08 08:21:07 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print "Hello World"
+print "My name is Carl. \nThis is Python"
+print 2015
+print "2015"
+print 3.1416
+
+print 2 + 2
+print 2 - 2 
+print 2 * 2 
+print 3 / 2 
+print 3 % 2 
+
+print 3.1416 * 2.0 
+print 3.1316 * 2 
