9e25f50a862f3f419759968bf5f04d53ac56f456
criemann <16criemann@iaa.edu.ec>
2015-05-08 08:24:18 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,13 @@
+
+operand1 = 95
+operand2 = 64.5
+
+
+print "95 + 64.5 = ", operand1+operand2
+print "95 - 64.5 = ", operand1-operand2
+
+print "95 * 64.5 = ", operand1*operand2
+
+print "95 / 64.5 = ", operand1/operand2
+
+print "95 % 64.5 = ", operand1%operand2
