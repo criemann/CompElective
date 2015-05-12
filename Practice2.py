b4292a59cbfea32e1168d3ba073ff3e9172814b2
criemann <16criemann@iaa.edu.ec>
2015-05-12 09:00:46 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,7 @@
+import sys
+
+print 'My name is' sys.argv[1]
+print 'My Last Name is' sys.argv[2]
+print 'My birthday is' sys.argv[3]
+print 'My favorite band is' sys.argv[4]
+print 'My favorite movie is' sys.argv[5]
